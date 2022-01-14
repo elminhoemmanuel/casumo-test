77a8f3a35fd5f02423fb73974a3f99128dfd4eb2
elminhoemmanuel <amodue15@gmail.com>
2022-01-14 10:35:07 +0100

    still working on casumo test

--- components/layout/modals/NewCard.js
+++ components/layout/modals/NewCard.js
@@ -11,28 +11,83 @@ const NewCard = () => {
     let types = ["mastercard", "visa"]
     let random = Math.floor(Math.random() * typesNumber)
     const [isDisabled, setIsDisabled,] = useState(true)
+    const [touchedName, setTouchedName,] = useState(false)
+    const [changed, setChanged,] = useState(0)
     const dispatch = useDispatch();
     const { showNew, showEdit } = useSelector((state) => state.cards);
-    const [details, setDetails] = useState({name:"", cardNumber:"", cvc:"", expiry:"", type:types[random]})
+    const [details, setDetails] = useState({ name: "", cardNumber: "", cvc: "", expiry: "", type: types[random] })
+    const [errors, setErrors] = useState({ name: "", cardNumber: "", cvc: "", expiry: "" })
+    const [touched, setTouched] = useState({ name: false, cardNumber: false, cvc: false, expiry: false })
 
-    // const handleCardNumber = (e) =>{
-    //     let text = e.target.value
-    //     let textArr = []
-    //     for (let i = 0; i < text.length; i++) {
-    //         console.log(i)
-    //         textArr.push(text[i])
-    //         if((i+1) % 4 === 0 && i+1 !== 16){
-    //             textArr.push(" ")
-    //         }
-            
-    //     }
-    //     console.log(textArr)
-        
-    //     setDetails({
-    //         ...details,
-    //         cardNumber:textArr.join("")
-    //     })
-    // }
+    useEffect(() => {
+        handleCardNumber(details)
+    }, [changed])
+
+    useEffect(() => {
+        if (details.name.length === 0 || details.cardNumber.length === 0 || details.expiry.length === 0 || details.cvc.length === 0) {
+            setIsDisabled(true)
+        } else {
+            setIsDisabled(false)
+        }
+    }, [details])
+
+    useEffect(() => {
+        validateName()
+    }, [touchedName])
+
+    function handleChange(evt) {
+        const value = evt.target.value;
+        setDetails({
+            ...details,
+            [evt.target.name]: value
+        });
+        // setTouched({
+        //     ...touched,
+        //     [evt.target.name]: true
+        // });
+
+    }
+
+    const cardNumHelper = (dets, val) => {
+        if (dets.cardNumber.length === val) {
+            let arr = dets.cardNumber.split("")
+            arr.splice(val - 1, 1);
+            setDetails({ ...dets, cardNumber: arr.join("") })
+        }
+    }
+
+    const cardExpHelper = (dets, val) => {
+        if (dets.expiry.length === val) {
+            let arr = dets.expiry.split("")
+            arr.splice(val - 1, 1);
+            setDetails({ ...dets, expiry: arr.join("") })
+        }
+    }
+
+
+    const handleCardNumber = (dets) => {
+        if (dets.cardNumber.length === 4 || dets.cardNumber.length === 9 || dets.cardNumber.length === 14) {
+            setDetails({ ...dets, cardNumber: dets.cardNumber + " " })
+        }
+        cardNumHelper(dets, 15);
+        cardNumHelper(dets, 10);
+        cardNumHelper(dets, 5);
+
+        if (dets.expiry.length === 2) {
+            setDetails({ ...dets, expiry: dets.expiry + "/" })
+        }
+        cardExpHelper(dets, 3);
+
+    }
+
+    const validateName = () => {
+        console.log(details.name.length)
+        if (details.name.length === 0) {
+            setErrors({ ...errors, name: "Please enter your name" })
+        } else {
+            setErrors({ ...errors, name: "none" })
+        }
+    }
 
     return (
         <div className="w-full h-screen z-30 absolute left-0 top-0 bg-black opacity-80 ">
@@ -41,7 +96,7 @@ const NewCard = () => {
                 <div className="absolute z-50 top-28 left-0 bg-white rounded-t-2xl px-6 md:px-10 pt-4 pb-6 text-casdark w-full h-screen">
                     <div className="mb-7 flex justify-end">
                         <div>
-                            <button onClick={()=>dispatch(toggleNew())} className="focus:outline-none">
+                            <button onClick={() => dispatch(toggleNew())} className="focus:outline-none">
                                 <FaTimes className="text-casdark h-6 w-6" />
                             </button>
                         </div>
@@ -56,16 +111,20 @@ const NewCard = () => {
                                     <label className="text-base" htmlFor="name">Name in card</label>
                                 </div>
 
-                                <div>
+                                <div className='relative'>
                                     <input
                                         value={details.name}
-                                        onChange={(e)=>{setDetails({...details,name:e.target.value})}}
+                                        onChange={(e) => { handleChange(e); }}
                                         name="name"
                                         className="w-full block focus:outline-none border-b border-casash py-2 pr-3 text-casash2 placeholder-casash"
                                         placeholder="John Doe"
                                         type="text"
                                     />
+                                    {errors.name !== "" && errors.name !== "none" && <img className='absolute top-2 right-0' src="/images/form-error.svg" alt="form error icon" />}
+                                    {errors.name === "none" && <img className='absolute top-3 right-0' src="/images/form-success.svg" alt="form success icon" />}
                                 </div>
+
+                                {errors.name !== "" && errors.name !== "none" && <p className='text-xs text-casred mt-1'>Please fill in your name</p>}
                             </div>
 
                             <div className="mb-7">
@@ -76,7 +135,7 @@ const NewCard = () => {
                                 <div>
                                     <input
                                         value={details.cardNumber}
-                                        // onChange={(e)=>{handleCardNumber(e)}}
+                                        onChange={(e) => { handleChange(e); setChanged(changed + 1); }}
                                         name="cardNumber"
                                         className="w-full block focus:outline-none border-b border-casash py-2 pr-3 text-casash2 placeholder-casash"
                                         placeholder="0000 0000 0000 0000"
@@ -93,9 +152,12 @@ const NewCard = () => {
 
                                 <div>
                                     <input
+                                        value={details.expiry}
+                                        onChange={(e) => { handleChange(e); setChanged(changed + 1); }}
                                         name="expiry"
                                         className="w-full block focus:outline-none border-b border-casash py-2 pr-3 text-casash2 placeholder-casash"
                                         placeholder="00/00"
+                                        maxLength="5"
                                     />
                                 </div>
                             </div>
@@ -107,9 +169,12 @@ const NewCard = () => {
 
                                 <div>
                                     <input
+                                        value={details.cvc}
+                                        onChange={(e) => { handleChange(e);setChanged(changed + 1); }}
                                         name="cvc"
                                         className="w-full block focus:outline-none border-b border-casash py-2 pr-3 text-casash2 placeholder-casash"
                                         placeholder="000"
+                                        maxLength="3"
                                     />
                                 </div>
                             </div>
